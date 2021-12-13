b8ede6a72ff4e864432da691da1a5a4582baed17
Jason <jrodriguez@simplenexus.com>
2021-12-13 09:53:59 -0700

    Adds encryption

---
+++ LTHPasscodeViewController/LTHKeychainUtils.swift
@@ -0,0 +1,9 @@
+//
+//  LTHKeychainUtils.swift
+//  LTHPasscodeViewController Demo
+//
+//  Created by Jason on 12/13/21.
+//  Copyright © 2021 Roland Leth. All rights reserved.
+//
+
+import Foundation
